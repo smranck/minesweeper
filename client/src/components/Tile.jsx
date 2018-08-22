795a14d0db40feb7b360235fcc564f1aad6ec651
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-08-21 23:20:29 -0700

    Create tiles for the game

---
+++ client/src/components/Tile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class Tile extends React.Component {
+  getValue() {
+    const { value } = this.props;
+
+    if (!value.isRevealed) {
+      return this.props.value.isFlagged ? 'F' : null;
+    }
+    if (value.isMine) {
+      return 'B';
+    }
+    if (value.neighbor === 0) {
+      return null;
+    }
+    return value.neightbor;
+  }
+
+  render() {
+    const { value, onClick, cMenu } = this.props;
+    return (
+      <div onClick={this.props.onClick} onContextMenu={this.props.cMenu}>
+        {this.getValue()}
+      </div>
+    );
+  }
+}
