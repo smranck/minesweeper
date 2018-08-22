39b2e89b3b1ad054a818d765cf45f3c72837b8e2
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-08-21 22:52:10 -0700

    Complete game component
    Which includes Board. Need that next.

--- client/src/components/game.jsx
+++ client/src/components/game.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default class Game extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    state = {
+      height: 8,
+      width: 8,
+      mines: 10,
+    };
+  }
+
+  render() {
+    const { height, width, mines } = this.state;
+    return (
+      <div className="game">
+        <Board height={height} width={width} mines={mines} />
+      </div>
+    );
+  }
 }
