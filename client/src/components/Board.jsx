80945b742a0aaf571646638e9b3c7a371ba54fbd
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-08-22 22:04:34 -0700

    Create function to find surrounding mines
    Also complete function that plants mines

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -40,8 +40,37 @@ export default class Board extends React.Component {
     while (minesPlanted < mines) {
       x = Math.floor(Math.random * width);
       y = Math.floor(Math.random * height);
+      if (!data[x][y].isMine) {
+        data[x][y].isMine = true;
+        minesPlanted += 1;
+      }
     }
   }
+
+  // function to look at a cell's neighbors to see if they are mines and remembers it
+  getNeighbors(data, height, width) {
+    let updatedData = data;
+    for (let i = 0; i < height; i += 1) {
+      for (let j = 0; j < width; j += 1) {
+        if (data[i][j].isMine !== true) {
+          let mine = 0;
+          const area = this.traverseBoard(data[i][j].x, data[i][j].y, data);
+          area.map((value) => {
+            if (value.isMine) {
+              mine += 1;
+            }
+          });
+          if (mine === 0) {
+            updatedData[i][j].isEmpty = true;
+          }
+          updatedData[i][j].neighbor = mine;
+        }
+      }
+    }
+    return updatedData;
+  }
+
+  // looks for neighboring cells and returns them
 
   render() {
     let { mines, gameStatus, boardData } = this.state;
