128ffd6aad888a269e5b0206ff8f8422e8187fe7
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-08-27 18:13:22 -0700

    Add styling for tiling
    Add prop-types for Value object

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -20,12 +20,31 @@ export default class Tile extends React.Component {
 
   render() {
     const { onClick, cMenu, value } = this.props;
+    const styles = {
+      tile: {
+        maxWidth: '400px',
+        margin: '0 auto',
+        padding: '20px',
+        background: '#7b7b7b',
+        border: '1px solid #fff',
+        float: 'left',
+        lineHeight: '45px',
+        height: '45px',
+        textAlign: 'center',
+        width: '45px',
+        cursor: 'pointer',
+        borderRadius: '5px',
+        color: '#fff',
+        fontWeight: '600',
+      },
+    };
     let className = `tile${value.isRevealed ? '' : ' hidden'}${value.isMine ? ' is-mine' : ''}${
       value.isFlagged ? ' is-flag' : ''
     }`;
     return (
-      <div className={className} onClick={onClick} onContextMenu={cMenu}>
+      <div style={styles.tile} className={className} onClick={onClick} onContextMenu={cMenu}>
         {this.getValue()}
+        Hello From Tile
       </div>
     );
   }
@@ -39,9 +58,19 @@ let alertErrs = () => {
 Tile.defaultProps = {
   onClick: alertErrs,
   cMenu: alertErrs,
+  value: alertErrs(),
 };
 
 Tile.propTypes = {
   onClick: PropTypes.func,
   cMenu: PropTypes.func,
+  value: PropTypes.shape({
+    isEmpty: PropTypes.bool,
+    isFlagged: PropTypes.bool,
+    isMine: PropTypes.bool,
+    isRevealed: PropTypes.bool,
+    neighbor: PropTypes.number,
+    x: PropTypes.number,
+    y: PropTypes.number,
+  }),
 };
