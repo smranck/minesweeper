10f72c8202f54b3dc54d30fa7076a0dafff58900
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-08-29 20:33:05 -0700

    Add mine functionality to right click handler

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -148,8 +148,8 @@ export default class Board extends React.Component {
     console.log('handling right click from Board.jsx');
     let { boardData, mineCount } = this.state;
     let updatedBoard = boardData.slice();
-    updatedBoard[x][y].isFlagged = true;
-    mineCount = Math.max(0, mineCount - 1);
+    updatedBoard[x][y].isFlagged = !updatedBoard[x][y].isFlagged;
+    updatedBoard[x][y].isFlagged ? mineCount -= 1 : mineCount += 1;
     this.setState({
       boardData: updatedBoard,
       mineCount,
