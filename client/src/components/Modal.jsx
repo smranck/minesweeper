356bbc9a5cac8aa5877802773977b1de01fc272a
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-09-28 17:35:00 -0700

    Create Modal to be used on game end

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Modal extends React.Component {
+  render() {
+    return <div>Hello Modal!</div>;
+  }
+}
