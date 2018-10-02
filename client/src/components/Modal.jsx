ca9d8e3669e3e3a5890d7317a7aac3cb7558798f
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-10-01 20:11:35 -0700

    Add buttons to Modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -4,7 +4,29 @@ import PropTypes from 'prop-types';
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Modal extends React.Component {
   render() {
-    const { gameState, newGame } = this.props;
-    return gameState === 3 ? <div>You Lost!</div> : <div> You Won!</div>;
+    const { gameState, newGame, toggleModal } = this.props;
+    return (
+      <div className="modal">
+        {gameState === 4 ? 'You Won!' : 'You Lost!'}
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            newGame();
+          }}
+        >
+          New Game
+        </button>
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            toggleModal();
+          }}
+        >
+          Close
+        </button>
+      </div>
+    );
   }
 }
